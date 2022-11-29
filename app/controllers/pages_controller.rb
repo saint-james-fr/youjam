03f5b8d3d682d73dad409981a3091ee043d22b71
Anne Ly <anne.ly59@gmail.com>
2022-11-29 18:30:47 +0100

    card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @jams = Jam.all.first(4).reverse
+    @users = User.all
   end
 
   def dashboard
