cf27673705d39a3479a2fb2f74f32de9fc1ab575
Anne Ly <anne.ly59@gmail.com>
2022-11-30 15:53:10 +0100

    tom select coming

--- app/controllers/jams_controller.rb
+++ app/controllers/jams_controller.rb
@@ -1,8 +1,17 @@
 class JamsController < ApplicationController
   def new
+    @jam = Jam.new
+    @instruments = Instrument.all.pluck(:name)
   end
 
   def create
+    @jam = Jam.new(params_jam)
+    @jam.user = current_user
+    if @jam.save
+      redirect_to jam_path(@jam)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -16,5 +25,11 @@ class JamsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def params_jam
+    params.require(:jam).permit(:location, :description, :capacity, :instruments_list, :jam_date, :title)
   end
 end
