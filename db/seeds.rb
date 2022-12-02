663c9f806c5ce9dd603b9a370f755aac1769969a
pierre petel <pierrepetel@gmail.com>
2022-12-02 15:16:45 +0100

    seeds ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-
 # !Reinitialization of data
 
 puts ">"
@@ -74,46 +73,132 @@ users_options = [user1, user2, user3, user4, user5]
 
 puts ">"
 puts "Starts creating users..."
+users = []
 users_options.each do |user_option|
-  file = URI.open("https://picsum.photos/200") #get avatar from Lorem Picsum
+  # file = URI.open("https://picsum.photos/200") #get avatar from Lorem Picsum
   user = User.new(user_option)
-  user.avatar.attach(io: file, filename: "nes#{rand(1..1000000)}.png", content_type: "image/png")
+  # user.avatar.attach(io: file, filename: "nes#{rand(1..1000000)}.png", content_type: "image/png")
   user.save!
+  users << user
 end
 puts ">"
 puts "Done!"
-
+puts "Starts attaching photos to users..."
+photo1 = URI.open('https://avatars.githubusercontent.com/u/107465113?v=4')
+users[0].avatar.attach(io: photo1, filename: "nes#{rand(1..1000000)}.png", content_type: "image/png")
+photo2 = URI.open("https://avatars.githubusercontent.com/u/112730785?v=4")
+users[1].avatar.attach(io: photo2, filename: "nes#{rand(1..1000000)}.png", content_type: "image/png")
+photo3 = URI.open("https://avatars.githubusercontent.com/u/107369283?v=4")
+users[2].avatar.attach(io: photo3, filename: "nes#{rand(1..1000000)}.png", content_type: "image/png")
+photo4 = URI.open("https://avatars.githubusercontent.com/u/36986044?v=4")
+users[3].avatar.attach(io: photo4, filename: "nes#{rand(1..1000000)}.png", content_type: "image/png")
+photo5 = URI.open("https://avatars.githubusercontent.com/u/114876925?v=4")
+users[4].avatar.attach(io: photo5, filename: "nes#{rand(1..1000000)}.png", content_type: "image/png")
+puts ">"
 # !Jams options & Seed
 
 jam1 =  {
-  title: "entrainons-nous ensemble",
+  title: "Entrainons-nous ensemble",
   location: "16 Villa Gaudelet, 75011 Paris",
-  description: "this is the famous wagon jam",
-  instruments_list: ["drums", "guitar", "bass"],
+  description: "La célèbre Jam du Wagon",
+  instruments_list: ["batterie", "guitare", "basse"],
   capacity: 3,
   jam_date: DateTime.strptime("12/07/2022 20:00", "%m/%d/%Y %H:%M"),
-  user_id: User.first.id
 }
 
 jam2 =  {
-  title: "je fais un solo, tu fais un solo",
+  title: "Je fais un solo, tu fais un solo",
   location: "48 avenue Parmentier, 75011 Paris",
-  description: "This is the Jam of the data versus Web Dev",
+  description: "La Jam de la Data versus Web Dev",
   instruments_list: ["drums", "guitar", "bass", "french horn", "saxophone"],
   capacity: 6,
   jam_date: DateTime.strptime("12/23/2022 20:00", "%m/%d/%Y %H:%M"),
-  user_id: User.last.id
 }
 
-jams_options = [jam1, jam2]
+jam3 =  {
+  title: "Clarinette au clair de Lune",
+  location: "42 rue de Maubeuge, 75011 Paris",
+  description: "After-Work Clarinette si ça vous dit ",
+  instruments_list: ["batterie", "guitare", "basse", "clarinette"],
+  capacity: 3,
+  jam_date: DateTime.strptime("12/23/2022 20:00", "%m/%d/%Y %H:%M"),
+}
+
+jam4 =  {
+  title: "Session Jazz",
+  location: "34 Boulevard Voltaire, 75011 Paris",
+  description: "Venez comme vous êtes",
+  instruments_list: ["batterie", "trompette", "basse", "saxophone", "contrebasse"],
+  capacity: 5,
+  jam_date: DateTime.strptime("12/01/2023 20:00", "%m/%d/%Y %H:%M"),
+}
+
+jam5 =  {
+  title: "Korn et Muse",
+  location: "12 Boulevard Marguerite de Rochechouard, 75011 Paris",
+  description: "Venez jouer les plus grands tubes de Muse et Korn",
+  instruments_list: ["batterie", "guitare", "basse", "kazoo"],
+  capacity: 4,
+  jam_date: DateTime.strptime("12/12/2022 18:00", "%m/%d/%Y %H:%M"),
+}
+
+jam6 =  {
+  title: "Christmas jam",
+  location: "12 Place de Clichy, 75008 Paris",
+  description: "nous somme une chorale de 2 et nous cherchons un orgue pour nous accompagner",
+  instruments_list: ["orgue"],
+  capacity: 3,
+  jam_date: DateTime.strptime("12/13/2022 17:00", "%m/%d/%Y %H:%M"),
+}
+
+jam7 =  {
+  title: "Old new wave",
+  location: "107 rue Saint Lazare, 75009 Paris",
+  description: "venez jouez du synthé sur nos textes enjoués ",
+  instruments_list: ["guitare", "synthétiseur",],
+  capacity: 2,
+  jam_date: DateTime.strptime("01/01/2023 10:00", "%m/%d/%Y %H:%M"),
+}
+
+jam8 =  {
+  title: "Trap Europ Express",
+  location: "50 rue des deux gares, 75010 Paris",
+  description: "je souhaiterai faire de la musique aussi bien influencée par la trap que Kraftwerk, rejoignez-moi !",
+  instruments_list: ["synthétiseur, batterie, vocoder","xylophone"],
+  capacity: 4,
+  jam_date: DateTime.strptime("12/20/2022 20:00", "%m/%d/%Y %H:%M"),
+}
+
+jam9 =  {
+  title: " Pump up the Jam",
+  location: "21 rue de Maubeuge, 75009 Paris",
+  description: "Je fais de l'eurodance et je cherche un joueur de triangle pour donner de l'âme à mes compos",
+  instruments_list: ["triangle"],
+  capacity: 2,
+  jam_date: DateTime.strptime("12/14/2022 20:00", "%m/%d/%Y %H:%M"),
+}
+
+jam10 =  {
+  title: " Edit path",
+  location: "4 place Pigalle",
+  description: "Pour les codeurs qui aiment les classiques de la chanson française",
+  instruments_list: ["piano", "trompette", "guitare"],
+  capacity: 5,
+  jam_date: DateTime.strptime("12/19/2022 20:00", "%m/%d/%Y %H:%M"),
+}
+
+jams_options = [jam1, jam2, jam3, jam4, jam5, jam6, jam7, jam8, jam9, jam10]
 
 puts ">"
 puts "Starts creating jams..."
+jams = []
 jams_options.each do |jam_option|
   file = URI.open("https://picsum.photos/1500") #get avatar from Lorem Picsum
   jam = Jam.new(jam_option)
+  jam.user = users.sample
   jam.photo.attach(io: file, filename: "nes#{rand(1..1000000)}.png", content_type: "image/png")
   jam.save!
+  jams << jam
 end
 puts ">"
 puts "Done!"
@@ -122,22 +207,26 @@ puts "Done!"
 
 booking1 =  {
   message: "salut j'ai grave envie de jammer, t'es chaud.e ?",
-  jam_id: Jam.first.id,
-  user_id: User.last.id
+
 }
 
 booking2 =  {
   message: "c'est l'heure de la jam! let's go!",
-  jam_id: Jam.last.id,
-  user_id: User.first.id
+
 }
 
-bookings_options = [booking1, booking2]
+booking3 =  {
+  message: "c'est l'heure de la jam! let's go!",
+}
+bookings_options = [booking1, booking2, booking3]
 
 puts ">"
 puts "Starts creating Bookings..."
 bookings_options.each do |booking_option|
+  jam = jams.sample
   booking = Booking.new(booking_option)
+  booking.jam = jam
+  booking.user = users.reject { |user| user.id == jam.user.id}.sample
   booking.save!
 end
 puts ">"
@@ -147,22 +236,33 @@ puts "Done!"
 
 post1 =  {
   content: "Ca va être mortel les petits loups",
-  jam_id: Jam.first.id,
-  user_id: User.last.id
 }
 
 post2 =  {
   content: "je ramène mon ampli de guitare",
-  jam_id: Jam.last.id,
-  user_id: User.first.id
+
 }
 
-post_options = [post1, post2]
+post3 =  {
+  content: "trop hâte !",
+
+}
+
+post4 =  {
+  content: "j'en ai un dans mon grenier !",
+
+}
+
+post_options = [post1, post2, post3, post4]
 
 puts ">"
 puts "Starts creating Posts..."
 post_options.each do |post_option|
+  jam = jams.sample
+  user = users.sample
   post = Post.new(post_option)
+  post.jam = jam
+  post.user = user
   post.save!
 end
 puts ">"
@@ -171,14 +271,12 @@ puts "Done!"
 # !Creations options & Seed
 
 creation1 =  {
-  name: "Mon dernier concert",
-  user_id: User.last.id,
+  name: "Je suis chaud !",
   creation_url: "https://www.youtube.com/watch?v=WaM2pxTPstw"
 }
 
 creation2 =  {
   name: "Mon dernier concert",
-  user_id: User.first.id,
   creation_url: "https://www.youtube.com/watch?v=WaM2pxTPstw"
 }
 
@@ -188,7 +286,12 @@ puts ">"
 puts "Starts creating Creations..."
 creations_options.each do |creation_option|
   creation = Creation.new(creation_option)
+  creation.user = users.sample
   creation.save!
 end
 puts ">"
+puts "Done!"
+
+puts ">"
+
 puts "Done!"
