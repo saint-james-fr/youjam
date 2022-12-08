8367205585b1c3af0990e365c38223ae0fdb17dd
pierre petel <pierrepetel@gmail.com>
2022-12-08 16:26:07 +0100

    edit-jam

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,10 +22,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:location, :description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:location, :description, :first_name, :last_name, :spotify_account, :instagram_url, :soundcloud_url])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:location, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:location, :description, :first_name, :last_name, :spotify_account, :instagram_url, :soundcloud_url])
   end
 
 
